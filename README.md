6fb94ef6ee8abb50c68db6afe954deb1a5224e42
alstent <rafaelalstent@gmail.com>
2023-08-10 14:48:15 +0200

    eslint-config-1.0

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# eslint-config
+# Rafel Alstent ESLint config
+
+## Whats included?
+
+- Standard config base;
+- React plugin;
+- React Hooks plugin;
+- Prettier;
+
+## Setup
+
+### React
+
+Install dependencies:
+```
+npm i -D eslint @alstent/eslint-config
+```
+Inside `.eslintrc.json`
+```
+{
+  "extends": [
+    "@alstent/eslint-config/react",
+  ]
+}
+```
+
+### Node.js
+
+coming soon...
---
+++ react.js
@@ -0,0 +1,66 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'standard',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  settings: { react: { version: '18.2' } },
+  plugins: [
+    'react-refresh',
+    'react',
+    'jsx-a11y',
+    'html'
+  ],
+  rules: {
+    'prettier/prettier': ["error", {
+      'printWidth': 100,
+      'tabWidth': 2,
+      'singleQuote': true,
+      'trailingComma': 'all',
+      'arrowParens': 'always',
+      'semi': false,
+      'endOfLine': 'auto',
+      'bracketSpacing': true
+    }],
+    'react/no-unescaped-entities': 'off',
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 'off',
+    'jsx-a11y/alt-text': [
+      'warn',
+      {
+        elements: ['img'],
+        img: ['Image'],
+      },
+    ],
+    'jsx-a11y/aria-props': 'warn',
+    'jsx-a11y/aria-proptypes': 'warn',
+    'jsx-a11y/aria-unsupported-elements': 'warn',
+    'jsx-a11y/role-has-required-aria-props': 'warn',
+    'jsx-a11y/role-supports-aria-props': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  }
+}
